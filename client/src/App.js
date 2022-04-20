eaeb06b14cdfbaed6013dea8451224ee674f7045
ngakana <nlsalemane@gmail.com>
2022-04-21 00:33:38 +0200

    🏗️ move models page to dedicated url

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import Model from "./pages/Model";
+import Models from "./pages/Models";
+import ModelDetails from "./pages/ModelDetails";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="
+    <div 
+    className="
+      relative
       max-h-screen w-screen bg-slate-900
       flex flex-col
     ">
@@ -17,9 +20,10 @@ function App() {
         <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Auth />} />
-            <Route path="/models/:id" element={<Model />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<Auth />} />
+            <Route path="/models" element={<Models />} />
+            <Route path="/models/:id" element={<ModelDetails />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
       </Router>   
