f0a935e3bc490684f64af10989503a3838a770ab
ngakana <nlsalemane@gmail.com>
2022-04-21 00:37:07 +0200

    add models page

---
+++ client/src/pages/Models.jsx
@@ -0,0 +1,13 @@
+
+function Models() {
+    return (
+        <div className="
+            h-screen w-screen
+            flex items-start justify-center
+        ">
+            <p className="text-center text-slate-500">List of models</p>
+        </div>
+    )
+}
+
+export default Models;
