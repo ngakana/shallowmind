7ecdee0f3720721541bbe75f32cbb4c56d66b594
ngakana <nlsalemane@gmail.com>
2022-04-21 00:35:56 +0200

    🚚 rename model.jsx to modeldetails.jsx

--- client/src/pages/ModelDetails.jsx
+++ client/src/pages/ModelDetails.jsx
@@ -1,13 +1,13 @@
 
-function Model() {
+function ModelDetails() {
     return (
         <div className="
             h-screen w-screen
             flex items-start justify-center
         ">
-            <p className="text-center text-slate-500">Model</p>
+            <p className="text-center text-slate-500">Model Details</p>
         </div>
     )
 }
 
-export default Model;
+export default ModelDetails;
