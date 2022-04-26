0be1f137241b47585f4c67a88b57d05e42ae7472
ngakana <nlsalemane@gmail.com>
2022-04-26 23:09:13 +0200

    add content

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import HeroBanner from "../components/HeroBanner";
 
 function Home() {
+
     return (
-        <div className="
+        <div
+            className="
             h-screen max-w-screen overflow-y-scroll overflow-x-hidden
             px-4
             flex flex-col items-center justify-start
@@ -13,7 +15,7 @@ function Home() {
                 heroText="AI computation models for human expertise! Topics:"
             />
             <p className="self-start text-xl text-slate-400 font-semibold pt-8">How we help businesses?</p>
-            <p className="text-justify text-slate-500 font-bold py-2">
+            <p className="text-justify text-slate-500 py-2">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Consectetur alias corrupti in dignissimos excepturi dolore
                 id velit magni hic eum quaerat blanditiis saepe asperiores
@@ -21,17 +23,27 @@ function Home() {
             </p>
             <Link
                 to="/models"
-                className="self-end bg-blue-400 px-4 py-2 text-md text-white rounded-full my-2"
+                className="self-end bg-blue-400 px-2 py-1 text-sm text-white rounded-full my-2"
             >
                 <p>See available models</p>
             </Link>
-            <p className="self-start text-xl text-slate-400 font-semibold pt-8">About ShallowMind</p>
-            <p className="text-justify text-slate-500 font-bold py-2">
+
+            <p className="self-start text-xl text-slate-400 font-semibold pt-8">How We Help Businesses</p>
+            <p className="text-justify text-slate-500 py-2">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Consectetur alias corrupti in dignissimos excepturi dolore
                 id velit magni hic eum quaerat blanditiis saepe asperiores
                 distinctio cupiditate.
             </p>
+
+            <p className="self-start text-xl text-slate-400 font-semibold pt-8">About ShallowMind</p>
+            <p className="text-justify text-slate-500 py-2">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur alias corrupti in dignissimos excepturi dolore
+                id velit magni hic eum quaerat blanditiis saepe asperiores
+                distinctio cupiditate.
+            </p>
+
         </div>
     )
 }
